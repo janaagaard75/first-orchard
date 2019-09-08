03dc824ca969d17f2ef66a05bc89435edcbb38a2
Jan Aagaard <jan@aagaard.net>
2019-09-08 10:47:36 +0200

    Functional main method

--- functional-typescript/src/Main.ts
+++ functional-typescript/src/Main.ts
@@ -6,7 +6,30 @@ import { RandomTreeStrategy } from "./strategies/RandomTreeStrategy"
 import { TreeWithFewestFruitsStrategy } from "./strategies/TreeWithFewestFruitsStategy"
 import { TreeWithMostFruitsStrategy } from "./strategies/TreeWithMostFruitsStategy"
 
-function run() {}
+function run() {
+  const strategies = [
+    new TreeWithFewestFruitsStrategy(),
+    new PreferredFruitStrategy(),
+    new RandomTreeStrategy(),
+    new TreeWithMostFruitsStrategy()
+  ]
+
+  const numberOfGamesPerStrategy = 100000
+  strategies
+    .map(strategy => {
+      return {
+        strategyName: strategy.name,
+        winPercentage: getWinPercentage(strategy, numberOfGamesPerStrategy)
+      }
+    })
+    .map(nameAndPercentage =>
+      getSummary(
+        nameAndPercentage.strategyName,
+        nameAndPercentage.winPercentage
+      )
+    )
+    .forEach(summary => console.info(summary))
+}
 
 function getWinPercentage(
   strategy: BasketStrategy,
@@ -26,43 +49,8 @@ function getWinPercentage(
   return Math.round((ourVictories / numberOfGames) * 100)
 }
 
-class Main {
-  constructor() {
-    this.strategies = [
-      new TreeWithFewestFruitsStrategy(),
-      new PreferredFruitStrategy(),
-      new RandomTreeStrategy(),
-      new TreeWithMostFruitsStrategy()
-    ]
-  }
-
-  private readonly numberOfGamesPerStrategy = 1000000
-  private readonly strategies: Array<BasketStrategy>
-
-  public run() {
-    this.strategies.forEach(strategy => {
-      const winPercentage = this.getWinPercentage(strategy)
-
-      console.info(
-        `We won ${winPercentage}% of the games with the '${strategy.name}' strategy.`
-      )
-    })
-  }
-
-  private getWinPercentage(strategy: BasketStrategy): number {
-    let ourVictories = 0
-
-    for (let i = 0; i < this.numberOfGamesPerStrategy; i++) {
-      const game = new Game(strategy)
-      game.playGame()
-
-      if (game.state === GameState.WeWon) {
-        ourVictories++
-      }
-    }
-
-    return Math.round((ourVictories / this.numberOfGamesPerStrategy) * 100)
-  }
+function getSummary(strategyName: string, winPercentage: number): string {
+  return `We won ${winPercentage}% of the games with the '${strategyName}' strategy.`
 }
 
-new Main().run()
+run()
